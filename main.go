69896323e47b01e0d5fe4960c21c34b21de576b0
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-11-12 10:28:11 -0300

    busca templates

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import(
 	"net/http"
+	"text/template"
 )
 
+var temp = template.Must(template.ParseGlob("/templates/*html"))
 
 func main(){
-	
+
 	http.ListenAndServe(":8000", nil)
 }

f4fcdc849b9babc4e7cb3b8eac9fc02bdf928a15
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-11-12 13:17:26 -0300

    cria funcao para pegar valores vindos do formulario

--- controllers/produtoController.go
+++ controllers/produtoController.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"cadastroProduto/models"
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
@@ -10,4 +12,27 @@ var temp = template.Must(template.ParseGlob("templates/*html"))
 
 func Index(w http.ResponseWriter, r *http.Request){
 	temp.ExecuteTemplate(w, "Index", models.GetAllProdutos())
+}
+
+func Insert(w http.ResponseWriter, r *http.Request){
+	if r.Method == "POST"{
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoFloat, err := strconv.ParseFloat(preco, 64)
+
+		if err !=nil{
+			log.Println("erro na conversao de preco")
+		}
+
+		quantidadeInt, err := strconv.Atoi(quantidade)
+
+		if err != nil{
+			log.Println("erro na conversao de quantidade")
+		}
+
+		models.CadastraProduto(nome, descricao, precoFloat, quantidadeInt)
+	}
 }
